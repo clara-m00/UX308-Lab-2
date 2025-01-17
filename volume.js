702c328ecc41152e8eee99e713962ea8fe56f123
clara-m00 <144965262+clara-m00@users.noreply.github.com>
2025-01-17 02:15:57 +0000

    add in units of measurement for volume.js

--- volume.js
+++ volume.js
@@ -1,11 +1,11 @@
 // This program *calculates* the **volume** of a **cylinder**
 
-let r = 6
+let r = 6;
 
-let diameter = r/2
+let diameter = r/2;
 
 let height = 10
 
 let volume = Math.PI * (diameter ** 2) * height;
 
-console.log(`The volume of a cylinder with a diameter of ${diameter} and a height of ${height} is ${volume} m³`);
+console.log(`The volume of a cylinder with a diameter of ${diameter}m and a height of ${height}m is ${volume}m³`);
