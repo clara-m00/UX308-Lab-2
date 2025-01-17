85a15338e2b718ec5ac90422f560aff3cadc73f3
clara-m00 <144965262+clara-m00@users.noreply.github.com>
2025-01-17 01:46:10 +0000

    add demo and questions 1 and 2

---
+++ km2miles.js
@@ -0,0 +1,7 @@
+// This program *converts* **kilometers** to **miles**
+
+let kilometers = 10;
+
+let miles = kilometers/1.609;
+
+console.log(`${kilometers} kilometers is ${miles} miles`);
---
+++ variable2letter.js
@@ -0,0 +1,33 @@
+// This program *converts* a **variable** to a **letter grade**
+
+let grade = 87
+
+if (grade < 50) {
+    console.log ("F");
+}
+
+else if (grade >= 50 && grade <= 59 ){
+    console.log ("D");
+}
+
+else if (grade >= 60 && grade <= 69){
+    console.log ("C");
+}
+
+else if (grade >= 70 && grade <= 79){
+    console.log ("B");
+}
+
+else if (grade >= 80 && grade <= 89){
+    console.log ("A");
+}
+
+else if (grade >= 90 && grade <= 99){
+    console.log ("A+");
+}
+
+else if (grade >= 100){
+    console.log ("Perfect!");
+}
+  
+        
