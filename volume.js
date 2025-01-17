8d5009e75d978e0334701cd10b42c9282d6e6a1c
clara-m00 <144965262+clara-m00@users.noreply.github.com>
2025-01-17 02:07:46 +0000

    add question 3 volume.js

--- volume.js
+++ volume.js
@@ -1,1 +1,11 @@
 // This program *calculates* the **volume** of a **cylinder**
+
+let r = 6
+
+let diameter = r/2
+
+let height = 10
+
+let volume = Math.PI * (diameter ** 2) * height;
+
+console.log(`The volume of a cylinder with a diameter of ${diameter} and a height of ${height} is ${volume} m³`);
